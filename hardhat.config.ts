63d22fef1f6206c44744e492bf0060b7abe655b9
nada <nada@alphamesh.xyz>
2025-03-04 14:05:50 +0800

    update: dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ignition-ethers";
+import "@nomicfoundation/hardhat-foundry"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
