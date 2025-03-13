b499d4c047eb09d11d29995dcce64fabc2a409ca
nada <nada@alphamesh.xyz>
2025-03-13 18:24:12 +0800

    fix: owner parameter

--- ignition/modules/Manager.ts
+++ ignition/modules/Manager.ts
@@ -1,8 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const ManagerModule = buildModule("ManagerModule", (m) => {
+  const deployer = m.getAccount(0);
   const manager = m.contract("Manager");
-  m.call(manager, "initialize", [m.getParameter("positionManager")]);
+  m.call(manager, "initialize", [deployer, m.getParameter("positionManager")]);
   return { manager }
 });
 
